769c53a2e4ae07088d64634f6b6a3337149f9021
qunying@kooboo.com <qunying@kooboo.com>
2017-07-12 09:16:49 +0800

    init

---
+++ components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="div">
+    <a>qwew</a> | 
+    <span>qwew</span> | 
+    <a>qwew</a> | 
+    <a>qwew</a> | 
+    <a>qwew</a> | 
+    <a>qwew</a> | 
+    <a class="btn btn-default">sdfds,</a>
+  </div>
+</template>
+<style lang="scss">
+.div {
+  a{
+  width: 100%;
+  }
+  span{
+    color:#00f;
+  }
+}
+</style>
+
